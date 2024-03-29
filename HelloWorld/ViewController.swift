3b69689f2b88ba3087eab995751d800ccdf8f6a7
Nikita <>
2024-03-29 14:58:59 +0200

    added label and button

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var textLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+    }
+    
+    
 }
 
