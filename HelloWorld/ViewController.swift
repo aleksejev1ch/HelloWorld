1943a401660c033516547078e46cb70e1fd13c73
Nikita <>
2024-03-29 15:15:08 +0200

    added functionality

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -10,16 +10,26 @@ import UIKit
 class ViewController: UIViewController {
     
     
+    @IBOutlet weak var toggleButton: UIButton!
     @IBOutlet weak var textLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        toggleButton.setTitle("Show Text", for: .normal)
+        textLabel.isHidden = true
+
     }
 
 
     @IBAction func buttonTapped(_ sender: Any) {
         
+        if textLabel.isHidden {
+            textLabel.isHidden = false
+            textLabel.text = "Hello World!"
+        } else {
+            textLabel.isHidden = true
+            toggleButton.setTitle("Hide Text", for: .normal)
+        }
     }
     
     
